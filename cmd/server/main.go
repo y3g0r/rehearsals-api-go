c122ed2eafb0ca05228ce36783c72f6a8b1e71d6
Yegor Ivashchenko <yegor.ivashchenko@protonmail.com>
2025-03-01 11:44:57 +0000

    Go-chi Getting started

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"github.com/y3g0r/rehearsals-api-go/internal/config"
-	"log"
 	"net/http"
-	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, Dockerized Go!")
-}
-
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
 	cfg := config.LoadConfig()
-	// Start the server
-	log.Printf("Starting server on %s", cfg.ServerAddress)
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(cfg.ServerAddress, nil)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+
+	http.ListenAndServe(cfg.ServerAddress, r)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/y3g0r/rehearsals-api-go
 
 go 1.24.0
+
+require github.com/go-chi/chi/v5 v5.2.1
